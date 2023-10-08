87481ad98366ca1615f8e3e80eac04237120c660
AlexVeeBee <alex200.veebee@gmail.com>
2023-10-08 15:17:29 +0000

    Add node package installer ( not working )

--- src/pages/checker.tsx
+++ src/pages/checker.tsx
@@ -4,6 +4,7 @@ import LoadingLine from "../UI/loadingline";
 import BasePage from "./_basepage"
 import { Command } from "@tauri-apps/api/shell"
 import { message } from "@tauri-apps/api/dialog"
+import { toast } from "react-hot-toast"
 
 const InstallChecker = () => {
     const [appStatus, setAppStatus] = useState<Status>("Paused");
@@ -95,13 +96,15 @@ const InstallChecker = () => {
 
     // sillytavern
     const [sillytavernStatus, setSillytavernStatus] = useState<Status>("Waiting");
+    const [sillytavernContext, setSillytavernContext] = useState<string>("git clone");
     const [sillytavernStatusLog, setsetSillytavernStatusLog] = useState<string[]>([]);
     const getSTlog = () => {
         return sillytavernStatusLog;
     }
     const pullSillytavern = async () => {
         try {
-            setSillytavernStatus("Downloading");
+            setSillytavernContext("git pull");
+            setSillytavernStatus("Update");
             const c = new Command("git-pull-st");
             console.log("pulling sillytavern", c);
             const r = await c.execute();
@@ -149,38 +152,97 @@ const InstallChecker = () => {
             return false;
         }
     }
+
+    // node packages
+    const [nodePackagesStatus, setNodePackagesStatus] = useState<Status>("Waiting");
+    const [nodePackagesStatusLog, setNodePackagesStatusLog] = useState<string[]>([]);
+    const installNodePackages = async () => {
+        // try {
+        //     console.log("changing directory to sillytavern");
+        //     const cd = new Command("cd-st",
+        //         ["cd", "jellytheme-Time-Machine"]
+        //     );
+        //     const r = await cd.execute();
+        //     console.log("changed directory to sillytavern", r);
+        // } catch (e) {
+        //     console.error("cd-st:", e);
+        //     setNodePackagesStatus("Error");
+        //     // @ts-ignore
+        //     setNodePackagesStatusLog([`cd-st: ${e.toString()}`]);
+        //     return false;
+        // }
+        try {
+            const npm = new Command("npm-install");
+            setNodePackagesStatus("Downloading");
+            console.log("installing node packages", npm);
+            const r = await npm.execute();
+            console.log("command executed: npm install", r);
+            if (r.code === 0) {
+                setNodePackagesStatus("Done");
+                return true;
+            } else {
+                setNodePackagesStatus("Error");
+                return false;
+            }
+        } catch (e) {
+            console.error("npm-install:", e);
+            // @ts-ignore
+            setNodePackagesStatusLog([`npm-install: ${e.toString()}`]);
+            setNodePackagesStatus("Error");
+            return false;
+        }
+    }
+
     const checkApps = async () => {
         const i_node = await NodeInstalled();
         const i_git = await gitinstalled();
         if (i_node && i_git) {
             const c_st = await cloneSillytavern();
             if (c_st) {
-                if (!localStorage.getItem("installed")) {
-                    localStorage.setItem("installed", "true");
-                    message("Ready to launch, please restart the launcher");
-                } else {
-                    router.navigate("/launcher");
+                const i_np = await installNodePackages();
+                if (i_np) {
+                    setAppStatus("Done");
+                    localStorage.removeItem("jump-to-checker");
+                    // router.navigate("/launcher");
                 }
+                if (!i_np) {
+                    setAppStatus("Error");
+                    return false;
+                }
+
+                // if (!localStorage.getItem("installed")) {
+                //     localStorage.setItem("installed", "true");
+                //     message("Ready to launch, please restart the launcher");
+                // } else {
+                //     localStorage.removeItem("jump-to-checker");
+                //     // router.navigate("/launcher");
+                // }
             }
         }
         if (!i_node || !i_git) {
             setAppStatus("Error");
+            return false;
         }
     }
     useEffect(() => {
         setAppStatus("Paused");
-        checkApps();
+        checkApps().then((b) => {
+            if (!b) {
+                toast.error("Error while preparing the launcher");
+            }
+        }).catch(() => {
+            setAppStatus("Error");
+        });
     }, [])
 
     return (
         <>
-        <LoadingLine />
+            <LoadingLine />
         <BasePage title={<p className={"title"}>Checking installation</p>}
             bodyStyle={{
                 marginTop: "24px",
             }}
         >
-
             <StatusWidget status={appStatus} statusText={"Launcher Update"} statusContent="updater"
                 // textlines={appStatusLog}
             />
@@ -190,8 +252,11 @@ const InstallChecker = () => {
             <StatusWidget status={gitStatus} statusText={"Git"} statusContent={gitStatusContext}
                 // textlines={gitTLogStatus}
             />
-            <StatusWidget status={sillytavernStatus} statusText={"Sillytavern"} statusContent="git clone"
+            <StatusWidget status={sillytavernStatus} statusText={"Sillytavern"} statusContent={sillytavernContext}
                 textlines={sillytavernStatusLog}
+            />
+            <StatusWidget status={nodePackagesStatus} statusText={"Node Packages"} statusContent="npm install"
+                textlines={nodePackagesStatusLog}
             />
             <p>A system restart may be required to complete the installation</p>
         </BasePage>
