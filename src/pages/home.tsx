7497463c535200d61c1fc1792f48f311d5cfd293
AlexVeeBee <alex200.veebee@gmail.com>
2023-10-08 15:15:25 +0000

    Make homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,56 @@
+import LoadingCircle from "../UI/LoadingCircle";
+import { JSX } from "preact";
+import Card from "../components/card";
+
+const Sidebar = ({
+    topButtons,
+    bottomButtons,
+    pages,
+}: {
+    topButtons: JSX.Element[],
+    bottomButtons: JSX.Element[],
+    pages: JSX.Element[],
+}) => {
+    return (
+        <div className="sidebar-container">
+            <div className="sidebar">
+                <div className="sidebar-top">
+                    {topButtons}
+                </div>
+                <div className="sidebar-bottom">
+                    {bottomButtons}
+                </div>
+            </div>
+            <div className="content">
+                {pages}
+            </div>
+        </div>
+    );
+}
+
 const LauncherDashboard = () => {
     return (
-        <div>
-            <h1>Launcher Dashboard</h1>
-        </div>
+        <Sidebar 
+            topButtons={[
+                <button>General</button>,
+                <button>Backups</button>,
+                <button>Toolbox</button>,
+                <button>Danger zone</button>,
+            ]}
+            bottomButtons={[
+                <button>Settings</button>,
+                <button>About</button>,
+            ]}
+            pages={[
+                <div className="page">
+                    <Card title="Placeholder">
+                        <div className="card-section">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                        </div>
+                    </Card>
+                </div>,
+            ]}
+        />
     );
 }
 
