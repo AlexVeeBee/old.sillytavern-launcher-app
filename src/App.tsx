3c4b6c7ec7036e1c014ba72b28df6541ac96d923
AlexVeeBee <alex200.veebee@gmail.com>
2023-10-08 15:17:45 +0000

    Updated App and added Upading to SVG component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import Setup from "./pages/setup";
 import InstallChecker from "./pages/checker";
 import LauncherDashboard from "./pages/home";
 import AppStart from "./pages/AppStart";
+import { Toaster, toast } from "react-hot-toast";
+import { useEffect } from "preact/compat";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <AppStart />
+  },
+  {
+    path: "/setup",
+    element: <Setup />
   },
   {
     path: "/checker",
@@ -31,6 +37,16 @@ function App() {
   return (
     <>
       <RouterProvider router={routes}/>
+      <Toaster
+        toastOptions={{
+          position: "bottom-right",
+          style: {
+            borderRadius: "3px",
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
     </>
   );
 }
--- src/SVG.tsx
+++ src/SVG.tsx
@@ -2,20 +2,23 @@
 const randid = () => Math.random().toString(36).substr(2, 9);
 
 const svgs = {
-    check: (<svg id={randid()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    check: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
     </svg>),
-    error: (<svg id={randid()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    error: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
     </svg>),  
-    downloading: (<svg id={randid()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    downloading: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
     </svg>),
-    waiting: (<svg id={randid()}xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    waiting: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>),
-    paused: (<svg id={randid()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    paused: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+    </svg>),
+    update: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
     </svg>)
 }
 
